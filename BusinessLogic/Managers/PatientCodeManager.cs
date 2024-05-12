9fe0a707d783815fbf518bf9badcd8e71593f890
Frederick <antoniobalderramamendoza@gmail.com>
2024-05-11 22:04:55 -0400

    P3-001 added BusinessLogic class library, set RootNamespace as UPB.BusinessLogic and UPB.Practice-3-cert-1, added Managers and Models folders in BusinessLogic as well as Models/PatientCode.cs and Managers/PatientCodeManager.cs files, CRUD functions implemented in PatientCodeManager.cs file

---
+++ BusinessLogic/Managers/PatientCodeManager.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using UPB.BusinessLogic.Models;
+
+namespace UPB.BusinessLogic.Managers
+{
+    internal class PatientCodeManager
+    {
+        private List<PatientCode> _patientCodes;
+
+        public PatientCodeManager()
+        {
+            _patientCodes = new List<PatientCode>();
+        }
+
+        public List<PatientCode> GetAll() { return _patientCodes; }
+
+        public PatientCode GetByCi(int ci)
+        {
+            PatientCode? foundPatientCode;
+
+            foundPatientCode = _patientCodes.Find(x => x.CI == ci);
+
+            if (foundPatientCode == null)
+            {
+                throw new NotImplementedException();
+            }
+
+            return foundPatientCode;
+        }
+
+        public void CreatePatientCode(PatientCode newPatientCode)
+        {
+            PatientCode createdPatientCode = new PatientCode()
+            {
+                Name = newPatientCode.Name,
+                LastName = newPatientCode.LastName,
+                CI = newPatientCode.CI,
+                Code = GeneratePatientCode(newPatientCode)
+            };
+
+            _patientCodes.Add(createdPatientCode);
+        }
+
+        public void UpdatePatientCode(int ci, PatientCode updatedPatientCode)
+        {
+            PatientCode? foundPatientCode;
+
+            foundPatientCode = _patientCodes.Find(x => x.CI == ci);
+
+            if (foundPatientCode == null)
+            {
+                throw new NotImplementedException();
+            }
+
+            foundPatientCode.Name = updatedPatientCode.Name;
+            foundPatientCode.LastName = updatedPatientCode.LastName;
+
+            foundPatientCode.Code = GeneratePatientCode(foundPatientCode);
+        }
+
+        public void DeletePatientCode(int ci)
+        {
+            PatientCode? foundPatientCode;
+
+            foundPatientCode = _patientCodes.Find(x => x.CI == ci);
+
+            if (foundPatientCode == null)
+            {
+                throw new NotImplementedException();
+            }
+
+            _patientCodes.Remove(foundPatientCode);
+        }
+
+        private string GeneratePatientCode(PatientCode patientCode)
+        {
+            string code;
+
+            code = $"{patientCode.Name[0]}{patientCode.LastName[0]}-{patientCode.CI}";
+
+            return code;
+        }
+    }
+}
---
+++ BusinessLogic/Models/PatientCode.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UPB.BusinessLogic.Models
+{
+    internal class PatientCode
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public int CI { get; set; }
+        public string Code { get; set; }
+
+        public PatientCode() { }
+
+        public PatientCode(string name, string lastName, int cI, string code)
+        {
+            Name = name;
+            LastName = lastName;
+            CI = cI;
+            Code = code;
+        }
+    }
+}

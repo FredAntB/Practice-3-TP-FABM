cbc20a5681c16692e884e222a560705cade9f267
Frederick <antoniobalderramamendoza@gmail.com>
2024-05-12 22:28:00 -0400

    P3-001 added Middlewares folder as well as Middlewares/ExceptionHandlerMiddleware.cs, installed serilog as well as serilog.sinks.File and serilog.sinks.Console, also started log register in ExceptionHandlerMiddleware.cs file

--- BusinessLogic/Managers/Exceptions/PatientCodeAlreadyExistsException.cs
+++ BusinessLogic/Managers/Exceptions/PatientCodeAlreadyExistsException.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace UPB.BusinessLogic.Managers.Exceptions
 {
-    internal class PatientCodeAlreadyExistsException : Exception
+    public class PatientCodeAlreadyExistsException : Exception
     {
         private const string BaseMessage = "There's already a Patient Code registered with the CI provided, please check if the provided information is correct, or update the patient's data.";
 
--- BusinessLogic/Managers/Exceptions/PatientCodeNotFoundException.cs
+++ BusinessLogic/Managers/Exceptions/PatientCodeNotFoundException.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace UPB.BusinessLogic.Managers.Exceptions
 {
-    internal class PatientCodeNotFoundException : Exception
+    public class PatientCodeNotFoundException : Exception
     {
         private const string BaseMessage = "The Patient Code you're looking for was not found.";
         public PatientCodeNotFoundException() : base(BaseMessage) { }
